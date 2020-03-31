b1130dea6fda6f0ccf1905ed698e13083d80fa5d
Griffin Poole <griffinpoole5@gmail.com>
2020-03-31 18:59:02 -0400

    variable declaration and basic operators

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -2,6 +2,9 @@ public class Hello {
 
     public static void main(String[] args){
         System.out.println("Hello, Griffin!");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        System.out.println(myFirstNumber);
     }
 
 }
