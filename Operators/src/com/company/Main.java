284b7748d536a90f30e9e41cb1940b61972c0cf9
Griffin Poole <griffinpoole5@gmail.com>
2020-03-31 21:46:23 -0400

    ternary operator

--- Operators/src/com/company/Main.java
+++ Operators/src/com/company/Main.java
@@ -74,10 +74,15 @@ public class Main {
         }
 
         boolean isCar = false;
-        if (isCar = true){
+        if (isCar){
             System.out.println("This is not supposed to happen");
         }
 
         //Ternary operator
+        isCar = true;
+        boolean wasCar = isCar ? true : false;
+        if(wasCar){
+            System.out.println("wasCar is true");
+        }
     }
 }
