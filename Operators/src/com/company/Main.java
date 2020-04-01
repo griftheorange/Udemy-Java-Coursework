27a7ae7a1ae30ac60c29943095b1410257acd527
Griffin Poole <griffinpoole5@gmail.com>
2020-03-31 21:36:06 -0400

    finished the comparison operators and and operators

--- Operators/src/com/company/Main.java
+++ Operators/src/com/company/Main.java
@@ -53,6 +53,16 @@ public class Main {
             System.out.println("And I am scared of aliens!");
         }
 
+        //Comparison Operator ==, !=, >, <, >=, <=
+        int topScore = 80;
+        if (topScore < 100){
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 60;
+        if (topScore > secondTopScore && topScore < 100){
+            System.out.println("Greater than second top score and less than 100");
+        }
 
     }
 }
