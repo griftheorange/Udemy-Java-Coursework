d6776bf7960b9dd1f1170a5ce96e25c023e4831c
Griffin Poole <griffinpoole5@gmail.com>
2020-03-31 20:57:29 -0400

    boolean, chars and unicode characters

--- CharAndBoolean/src/com/company/Main.java
+++ CharAndBoolean/src/com/company/Main.java
@@ -3,6 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //Char type occupies two bytes 16-bit: Unicode Characters
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+
+        //Boolean Values
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
     }
 }
