ed70d59e526437d3a0451fb9660fb04f61bba2b5
Griffin Poole <griffinpoole5@gmail.com>
2020-03-31 20:11:05 -0400

    Byte, Short and Long Learned

--- ByteShortIntLong/src/com/company/Main.java
+++ ByteShortIntLong/src/com/company/Main.java
@@ -4,13 +4,33 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int myValue = 10000;
+        //Byte Range Testing 8-bits
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+        System.out.println("Byte Minimum Value = " + myMaxByteValue);
+
+        //Long Range Testing 16-bits
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Minimum Value = " + myMinShortValue);
+        System.out.println("Short Minimum Value = " + myMaxShortValue);
+
+        //Integer Range Testing 32-bits
         int myMinIntValue = Integer.MIN_VALUE;
         int myMaxIntValue = Integer.MAX_VALUE;
-
         System.out.println("Integer Minimum Value = " + myMinIntValue);
         System.out.println("Integer Maximum Value = " + myMaxIntValue);
         System.out.println("Busted MAX Value = " + (myMaxIntValue + 1));
         System.out.println("Busted MIN Value = " + (myMinIntValue - 1));
+
+        //Long Range Testing 64-bits
+        long myLongValue = 100L;
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        System.out.println("Long Minimum Value = " + myMaxLongValue);
+        long bigLongLiteralValue = 2_147_483_647_234L;
+        System.out.println(bigLongLiteralValue);
     }
 }
