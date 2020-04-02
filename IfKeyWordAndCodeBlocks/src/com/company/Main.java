1b5553238a31e925a44a3a3fab748cb7af55487e
Griffin Poole <griffinpoole5@gmail.com>
2020-04-02 01:02:19 -0400

    if-else control block complete

--- IfKeyWordAndCodeBlocks/src/com/company/Main.java
+++ IfKeyWordAndCodeBlocks/src/com/company/Main.java
@@ -8,9 +8,28 @@ public class Main {
         int levelCompleted = 5;
         int bonus = 100;
 
-        if (score == 5000) {
+        if (score <= 5001 && score >= 1000) {
             System.out.println(String.format("Your score was %1$s, your bonus was %2$s", score, bonus));
             System.out.println("This was executed");
+        } else if(score < 1000){
+            System.out.println("The score was less than 1000");
+        } else {
+            System.out.println("Got here");
+        }
+
+        if (gameOver == true){
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+
+        if (gameOver == true){
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
         }
     }
 }
