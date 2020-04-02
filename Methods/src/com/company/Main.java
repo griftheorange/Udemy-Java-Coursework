977b97e42e3f7ec231ec8796a1edb561107f8699
Griffin Poole <griffinpoole5@gmail.com>
2020-04-02 19:16:59 -0400

    completed methods 2

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -19,9 +19,42 @@ public class Main {
         if(gameOver){
             System.out.println("Your final score was " + calculateScore(score, levelCompleted, bonus));
         }
+
+        String name = "Griffin";
+
+        score = 1500;
+        displayHighScorePosition(name, calculateHighScorePosition(score));
+
+        score = 900;
+        displayHighScorePosition(name, calculateHighScorePosition(score));
+
+        score = 400;
+        displayHighScorePosition(name, calculateHighScorePosition(score));
+
+        score = 50;
+        displayHighScorePosition(name, calculateHighScorePosition(score));
+
+        score = 1000;
+        displayHighScorePosition(name, calculateHighScorePosition(score));
     }
 
     public static int calculateScore(int score, int levelCompleted, int bonus) {
         return (score + (levelCompleted * bonus));
+    }
+
+    public static void displayHighScorePosition(String name, int position){
+        System.out.println(String.format("%1$s managed to get into position %2$s on the high score table.", name, position));
+    }
+
+    public static int calculateHighScorePosition(int score){
+        if(score >= 1000){
+            return 1;
+        } else if(score >= 500){
+            return 2;
+        } else if(score >= 100){
+            return 3;
+        } else {
+            return 4;
+        }
     }
 }
