fd8ba9e5e360846015b1a406bb6166a46db5fc81
Griffin Poole <griffinpoole5@gmail.com>
2020-04-03 19:37:32 -0400

    switch challengr Day of the week

--- DayOfTheWeek/src/com/company/Main.java
+++ DayOfTheWeek/src/com/company/Main.java
@@ -7,32 +7,32 @@ public class Main {
     }
 
     public static void printDayOfTheWeek(int day){
-        if(day >= 0 && day <= 6){
-            switch(day){
-                case 0:
-                    System.out.println("Sunday");
-                    break;
-                case 1:
-                    System.out.println("Monday");
-                    break;
-                case 2:
-                    System.out.println("Tuesday");
-                    break;
-                case 3:
-                    System.out.println("Wednesday");
-                    break;
-                case 4:
-                    System.out.println("Thursday");
-                    break;
-                case 5:
-                    System.out.println("Friday");
-                    break;
-                default:
-                    System.out.println("Saturday");
-                    break;
-            }
-        } else {
-            System.out.println("Invalid day");
+        switch(day){
+            case 0:
+                System.out.println("Sunday");
+                break;
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Invalid value");
+                break;
         }
     }
 }
+
