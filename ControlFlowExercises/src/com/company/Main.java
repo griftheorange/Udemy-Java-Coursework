182565ed114d8e1c8bbcc307f66c2b316b4d5d8a
Griffin Poole <griffinpoole5@gmail.com>
2020-04-03 19:57:55 -0400

    finished days in month calss

---
+++ ControlFlowExercises/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        NumberInWord.printNumberInWord(7);
+
+        System.out.println(NumberOfDaysInMonth.getDaysInMonth(2, 2020));
+    }
+}
