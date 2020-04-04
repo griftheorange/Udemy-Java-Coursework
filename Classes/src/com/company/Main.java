794eab5b06bb63e0b4308b9b687ed465c0525bd2
Griffin Poole <griffinpoole5@gmail.com>
2020-04-04 19:45:05 -0400

    Class getter setters complete

--- Classes/src/com/company/Main.java
+++ Classes/src/com/company/Main.java
@@ -5,9 +5,10 @@ public class Main {
     public static void main(String[] args) {
         Car porsche = new Car();
         Car holden = new Car();
-        porsche.model = "Carrera";
+        System.out.println(String.format("Model is %s", porsche.getModel()));
+        porsche.setModel("Carrera");
         System.out.println(porsche.equals(holden));
         System.out.println(porsche.equals(porsche));
-        System.out.println(porsche.model);
+        System.out.println(String.format("Model is %s", porsche.getModel()));
     }
 }
