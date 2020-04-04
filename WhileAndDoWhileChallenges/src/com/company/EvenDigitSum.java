b0668947fd66fdca947e4900ff5fbd9506f71c68
Griffin Poole <griffinpoole5@gmail.com>
2020-04-03 21:15:02 -0400

    Even digit sum complete

---
+++ WhileAndDoWhileChallenges/src/com/company/EvenDigitSum.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class EvenDigitSum {
+    public static int getEvenDigitSum(int num){
+        if(num >= 0){
+            int sum = 0;
+            while(num > 0){
+                int digit = num % 10;
+                num /= 10;
+                if(digit % 2 == 0){
+                    sum += digit;
+                }
+            }
+            return sum;
+        } else {
+            return -1;
+        }
+    }
+}
--- WhileAndDoWhileChallenges/src/com/company/Main.java
+++ WhileAndDoWhileChallenges/src/com/company/Main.java
@@ -5,14 +5,24 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(DigitSum.sumDigits(125));
 
+        System.out.println();
+
         System.out.println(NumberPalindrome.isPalindrome(-1221));
         System.out.println(NumberPalindrome.isPalindrome(707));
         System.out.println(NumberPalindrome.isPalindrome(11212));
+
+        System.out.println();
 
         System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(252));
         System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(257));
         System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(0));
         System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(5));
         System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(-10));
+
+        System.out.println();
+
+        System.out.println(EvenDigitSum.getEvenDigitSum(123456789));
+        System.out.println(EvenDigitSum.getEvenDigitSum(252));
+        System.out.println(EvenDigitSum.getEvenDigitSum(-22));
     }
 }
