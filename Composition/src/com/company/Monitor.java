0af4a51a6851ca15441c30ca5e48f446ca16b85b
Griffin Poole <griffinpoole5@gmail.com>
2020-04-04 23:14:38 -0400

    Composition part 1 complete

---
+++ Composition/src/com/company/Monitor.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Monitor {
+
+    private String model;
+    private String manufacturer;
+    private int size;
+    private Resolution nativeResolution;
+
+    public Monitor(String model, String manufacturer, int size, Resolution nativeResolution) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.size = size;
+        this.nativeResolution = nativeResolution;
+    }
+
+    public void drawPixelAt(int x, int y, String color){
+        System.out.println(String.format("Drawing pixel at %1$s %2$s in color %3$s", x, y, color));
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Resolution getNativeResolution() {
+        return nativeResolution;
+    }
+}
