e4bcf18d208b97116cd6b7cb8361b1ead6851383
Griffin Poole <griffinpoole5@gmail.com>
2020-04-05 21:21:49 -0400

    Polymorphism complete

---
+++ Polymorphism/src/com/company/Main.java
@@ -0,0 +1,99 @@
+package com.company;
+
+abstract class Movie {
+    private String name;
+
+    public Movie(String name){
+        this.name = name;
+    }
+
+    public String plot(){
+        return "No plot here";
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Jaws extends Movie {
+    public Jaws() {
+        super("Jaws");
+    }
+
+    @Override
+    public String plot(){
+        return "A shark eats lots of people";
+    }
+}
+
+class IndependenceDay extends Movie{
+    public IndependenceDay() {
+        super("Independence Day");
+    }
+
+    @Override
+    public String plot() {
+        return "Aliens attempt to take over Earth";
+    }
+}
+
+class MazeRunner extends Movie {
+    public MazeRunner() {
+        super("Maze Runner");
+    }
+
+    @Override
+    public String plot() {
+        return "Kids try to escape a maze";
+    }
+}
+
+class StarWars extends Movie{
+    public StarWars(){
+        super("Star Wars");
+    }
+
+    public String plot(){
+        return "Imperial Forces try to take over the universe";
+    }
+}
+
+class Forgetable extends Movie {
+    public Forgetable() {
+        super("Forgetable");
+    }
+
+    //No plot method
+}
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        for(int i=1; i < 11; i++){
+            Movie movie = randomMovie();
+            System.out.println(String.format("Movie #%1$s: %2$s", i, movie.getName()));
+            System.out.println(String.format("Plot: %s", movie.plot()));
+            System.out.println();
+        }
+    }
+
+    public static Movie randomMovie(){
+        int randomNumber = (int)(Math.random() * 5)+1;
+        System.out.println("Number generated was " + randomNumber);
+        switch(randomNumber){
+            case 1:
+                return new Jaws();
+            case 2:
+                return new IndependenceDay();
+            case 3:
+                return new MazeRunner();
+            case 4:
+                return new StarWars();
+            default:
+                return new Forgetable();
+        }
+    }
+}
