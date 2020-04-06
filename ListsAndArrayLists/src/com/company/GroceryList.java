982663385b71c48f4f45451c423b86a8aa552106
Griffin Poole <griffinpoole5@gmail.com>
2020-04-06 00:04:31 -0400

    Finished arraylists part 1

---
+++ ListsAndArrayLists/src/com/company/GroceryList.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class GroceryList {
+    private int[] myNumebers = new int[50];
+    private ArrayList<String> groceryList = new ArrayList<String>();
+
+    public void addGroceryItem(String item){
+        groceryList.add(item);
+        System.out.println("You have " + groceryList.size() + " items in your list");
+        for(int i = 0; i < groceryList.size(); i++){
+            System.out.println((i+1) + ". " + groceryList.get(i));
+        }
+    }
+
+    public void modifyGroceryItem(int position, String newItem){
+        groceryList.set(position, newItem);
+        System.out.println(String.format("Grocery item %1$s has been set to %2$s", position, newItem));
+    }
+
+    public void removeGroceryItem(int position){
+        String theItem = groceryList.get(position);
+        groceryList.remove(position);
+        System.out.println(String.format("%s removed from list", theItem));
+    }
+}
