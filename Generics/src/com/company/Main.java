8fd4a9d00d7211b9e239f46ad146766e4eba3d0d
Griffin Poole <griffinpoole5@gmail.com>
2020-04-10 17:36:59 -0400

    Initialized base code for Generics

--- Generics/src/com/company/Main.java
+++ Generics/src/com/company/Main.java
@@ -5,22 +5,32 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Integer> items = new ArrayList<>();
-        items.add(1);
-        items.add(2);
-        items.add(3);
-//        items.add("Yeet");
-        items.add(4);
-        items.add(5);
+        FootballPlayer joe = new FootballPlayer("Joe");
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+        SoccerPlayer beckham = new SoccerPlayer("Beckham");
 
-        printDoubled(items);
+        Team adelaideCrows = new Team("Adelaide Crows");
+        adelaideCrows.addPlayer(joe);
+        adelaideCrows.addPlayer(pat);
+        adelaideCrows.addPlayer(beckham);
+
+        System.out.println(adelaideCrows.numPlayers());
+//        ArrayList<Integer> items = new ArrayList<>();
+//        items.add(1);
+//        items.add(2);
+//        items.add(3);
+////        items.add("Yeet");
+//        items.add(4);
+//        items.add(5);
+//
+//        printDoubled(items);
     }
 
-    private static void printDoubled(ArrayList<Integer> n){
-        if(n != null){
-            for(int i : n){
-                System.out.println(i*2);
-            }
-        }
-    }
+//    private static void printDoubled(ArrayList<Integer> n){
+//        if(n != null){
+//            for(int i : n){
+//                System.out.println(i*2);
+//            }
+//        }
+//    }
 }
