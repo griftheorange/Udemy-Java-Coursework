8391243fab987d79c05cd5ccf0db467046445467
Griffin Poole <griffinpoole5@gmail.com>
2020-04-10 20:52:45 -0400

    Packages part 1 complete

---
+++ Packages/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.w3c.dom.Node;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Node node = null;
+        Scanner scanner = new Scanner(System.in);
+    }
+}
