2233a4213e421f4e35140e160c8c699714910c61
Griffin Poole <griffinpoole5@gmail.com>
2020-04-10 22:46:04 -0400

    Finishshed first scope lesson

---
+++ Scope/src/com/griffin/Series/ScopeCheck.java
@@ -0,0 +1,37 @@
+package com.griffin.Series;
+
+public class ScopeCheck {
+    public int publicVar = 0;
+    private int privateVar = 1;
+
+    public ScopeCheck(){
+        System.out.println("ScopeCheck created, publicVar = " + publicVar + ": privateVar = " + privateVar);
+    }
+
+    public int getPrivateVar() {
+        return privateVar;
+    }
+
+    public void timesTwo(){
+        int privateVar = 2;
+        for(int i = 0; i < 10; i++){
+            System.out.println(i + " times two is " + i * privateVar);
+        }
+//        System.out.println("Value of i is now " + i);
+    }
+
+    public class InnerClass {
+        public int privateVar = 3;
+
+        public InnerClass(){
+            System.out.println("InnerClass created, privateVar is " + privateVar);
+        }
+
+        public void timesTwo(){
+            for(int i = 0; i < 10; i++){
+                System.out.println(i + " times two is " + i * privateVar);
+            }
+//        System.out.println("Value of i is now " + i);
+        }
+    }
+}
