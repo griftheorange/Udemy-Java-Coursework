b323958ba68eefa56e7bc04c0ed788d0b5b9ad40
Griffin Poole <griffinpoole5@gmail.com>
2020-04-11 18:20:52 -0400

    Scope part 2 complete

--- Scope/src/com/griffin/Series/Main.java
+++ Scope/src/com/griffin/Series/Main.java
@@ -3,16 +3,22 @@ package com.griffin.Series;
 public class Main {
 
     public static void main(String[] args) {
-        String privateVar = "this is private to main()";
+        String varFour = "this is private to main()";
 
         ScopeCheck scopeInstance = new ScopeCheck();
-        System.out.println("scopeInstance privateVar is " + scopeInstance.getPrivateVar());
-        System.out.println(privateVar);
+        System.out.println("scopeInstance varOnce is " + scopeInstance.getVarOne());
+        System.out.println(varFour);
 
         scopeInstance.timesTwo();
         System.out.println("*************************");
         ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
         innerClass.timesTwo();
+        System.out.println();
+
+        scopeInstance.useInner();
+        System.out.println();
+        ScopeCheck.InnerClass innerClass1 = scopeInstance.new InnerClass();
+        System.out.println("varThree is not accessible here");
 
     }
 }
