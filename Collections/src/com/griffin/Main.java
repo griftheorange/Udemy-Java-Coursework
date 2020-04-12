dc2376c3e78563b4439cdc27ea2e8da7f8e1dff7
Griffin Poole <griffinpoole5@gmail.com>
2020-04-12 04:15:37 -0400

    Settup Class structure 'Theatre' for reformatting using Collections Framework

---
+++ Collections/src/com/griffin/Main.java
@@ -0,0 +1,19 @@
+package com.griffin;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Theater sydney = new Theater("Sydney Opera House", 20, 50);
+//        sydney.getSeats();
+        if(sydney.reserveSeat("H11")){
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Sorry this seat is taken");
+        }
+        if(sydney.reserveSeat("H11")){
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Sorry this seat is taken");
+        }
+    }
+}
