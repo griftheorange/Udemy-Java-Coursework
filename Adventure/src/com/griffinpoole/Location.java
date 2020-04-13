2d1372d760a709f88b9e1382f25aa798bfc76d97
Griffin Poole <griffinpoole5@gmail.com>
2020-04-13 17:52:43 -0400

    Immutable class settup

--- Adventure/src/com/griffinpoole/Location.java
+++ Adventure/src/com/griffinpoole/Location.java
@@ -3,21 +3,21 @@ package com.griffinpoole;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Location {
+public final class Location {
     private final int locationID;
     private final String description;
     private final Map<String, Integer> exits;
 
-    public Location(int locationID, String description) {
+    public Location(int locationID, String description, Map<String, Integer> exits) {
         this.locationID = locationID;
         this.description = description;
-        this.exits = new HashMap<>();
+        this.exits = new HashMap<>(exits);
         this.exits.put("Q", 0);
     }
 
-    public void addExit(String direction, int location){
-        exits.put(direction, location);
-    }
+//    public void addExit(String direction, int location){
+//        exits.put(direction, location);
+//    }
 
     public int getLocationID() {
         return locationID;
