31eee6e7c14032342205fac54c052f9bc3abd741
Griffin Poole <griffinpoole5@gmail.com>
2020-04-13 15:00:13 -0400

    Map Collection review complete

--- Collections/src/com/griffin/MapProgram.java
+++ Collections/src/com/griffin/MapProgram.java
@@ -16,7 +16,7 @@ public class MapProgram {
         languages.put("Python", "An interpreted, object-oriented, high-level programming language with dynamic semantics");
         languages.put("Algol", "An algorithmic language");
         System.out.println(languages.put("BASIC", "Beginners All Purpose Symbolic Instruction Code"));
-        System.out.println(languages.put("List", "Hherein lies madness"));
+        System.out.println(languages.put("Lisp", "Herein lies madness"));
 
         System.out.println(languages.get("Ruby"));
         if(languages.containsKey("Java")){
@@ -28,6 +28,19 @@ public class MapProgram {
         System.out.println(languages.get("Java"));
 
         System.out.println("==================================================");
+
+//        languages.remove("Lisp");
+        if(languages.remove("Algol", "A family of algorithmic languages")){
+            System.out.println("Algol removed");
+        } else {
+            System.out.println("Algol not removed, key/value pair not found");
+        }
+        if(languages.replace("Lisp", "Herein lies madness", "a functional programming language with imperative features")){
+            System.out.println("Lisp replaced");
+        } else {
+            System.out.println("Lisp was not replaced");
+        }
+        System.out.println(languages.replace("Scala", "This will not be added"));
         for(String key : languages.keySet()){
             System.out.println(String.format("%1$s: %2$s", key, languages.get(key)));
         }
