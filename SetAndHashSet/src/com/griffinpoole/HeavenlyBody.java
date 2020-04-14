9bef1be7ff6d698514a7ad23f2c3592d575e7a3a
Griffin Poole <griffinpoole5@gmail.com>
2020-04-14 07:08:34 -0400

    Implemented Key inner class for HeavenlyBody that allows for proper allowcation in a map data structure

--- SetAndHashSet/src/com/griffinpoole/HeavenlyBody.java
+++ SetAndHashSet/src/com/griffinpoole/HeavenlyBody.java
@@ -1,13 +1,13 @@
 package com.griffinpoole;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public abstract class HeavenlyBody {
-    private final String name;
     private final double orbitalPeriod;
     private final Set<HeavenlyBody> satellites;
-    private final BodyTypes bodyType;
+    private final Key key;
 
     public enum BodyTypes {
         STAR,
@@ -19,14 +19,17 @@ public abstract class HeavenlyBody {
     }
 
     public HeavenlyBody(String name, double orbitalPeriod, BodyTypes bodyType) {
-        this.name = name;
+        this.key = new Key(name, bodyType);
         this.orbitalPeriod = orbitalPeriod;
         this.satellites = new HashSet<>();
-        this.bodyType = bodyType;
     }
 
     public String getName() {
-        return name;
+        return this.key.getName();
+    }
+
+    public Key getKey() {
+        return key;
     }
 
     public double getOrbitalPeriod() {
@@ -42,12 +45,16 @@ public abstract class HeavenlyBody {
     }
 
     public BodyTypes getBodyType() {
-        return bodyType;
+        return this.key.getBodyType();
+    }
+
+    public static Key makeKey(String name, BodyTypes bodyType){
+        return new Key(name, bodyType);
     }
 
     @Override
     public String toString() {
-        return this.name + ": " + this.bodyType + ", " + this.orbitalPeriod;
+        return this.key.name + ": " + this.key.bodyType + ", " + this.orbitalPeriod;
     }
 
     @Override
@@ -57,15 +64,52 @@ public abstract class HeavenlyBody {
         }
         if(obj instanceof HeavenlyBody){
             HeavenlyBody theObject = (HeavenlyBody) obj;
-            if(this.name.equals(theObject.getName())){
-                return this.bodyType.equals(theObject.getBodyType());
-            }
+            return this.key.equals(theObject.getKey());
         }
         return false;
     }
 
     @Override
     public final int hashCode() {
-        return this.name.hashCode() + 57 + this.bodyType.hashCode();
+        return this.key.hashCode();
+    }
+
+    public static final class Key {
+        private String name;
+        private BodyTypes bodyType;
+
+        private Key(String name, BodyTypes bodyType){
+            this.name = name;
+            this.bodyType = bodyType;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public BodyTypes getBodyType() {
+            return bodyType;
+        }
+
+        @Override
+        public int hashCode() {
+            return this.name.hashCode() + 57 + this.bodyType.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            Key key = (Key) obj;
+            if (obj instanceof Key){
+                if(this.name.equals(key.getName())){
+                    return (this.bodyType == key.getBodyType());
+                }
+            }
+            return false;
+        }
+
+        @Override
+        public String toString() {
+            return this.name + ": " + this.bodyType;
+        }
     }
 }
