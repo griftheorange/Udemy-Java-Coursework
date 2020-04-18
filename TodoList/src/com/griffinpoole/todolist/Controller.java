933bf94af1ceb9880c155f77d9e2b0cbf7a113e7
Griffin Poole <griffinpoole5@gmail.com>
2020-04-18 12:23:14 -0400

    Created singleton class TodoData responsible for saving and loading user data on open and close of app

--- TodoList/src/com/griffinpoole/todolist/Controller.java
+++ TodoList/src/com/griffinpoole/todolist/Controller.java
@@ -28,7 +28,7 @@ public class Controller {
     public void initialize(){
         TodoItem item1 = new TodoItem("Mail birthday card", "Buy a 30th birthday card for John", LocalDate.of(2020, Month.JULY, 15));
         TodoItem item2 = new TodoItem("Doctors Appointment", "See Dr. Smith at 123 Main Street. Bring paperwork", LocalDate.of(2020, Month.MAY, 23));
-        TodoItem item3 = new TodoItem("Finish desing proposal for FZ", "Promised FZ I;d storyboard the game by Friday", LocalDate.of(2020, Month.APRIL, 30));
+        TodoItem item3 = new TodoItem("Finish design proposal for FZ", "Promised FZ I'd storyboard the game by Friday", LocalDate.of(2020, Month.APRIL, 30));
         TodoItem item4 = new TodoItem("Feed Willow and Maple", "Need to feed them vegetables, all the vegetables. Growing Guinea Pigs need a whole lot of vegetables", LocalDate.of(2020, Month.APRIL, 20));
         TodoItem item5 = new TodoItem("Clean room", "Dishes piling up again, need to bus them downstairs", LocalDate.of(2020, Month.DECEMBER, 15));
 
