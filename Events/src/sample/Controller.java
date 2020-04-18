b7ae2a4b5c6812f968ddd6816beaf0bb03f5e7b6
Griffin Poole <griffinpoole5@gmail.com>
2020-04-18 07:36:24 -0400

    Added checkbox for text view clear on print, similuated freeze-up of UI Thread with .sleep()

--- Events/src/sample/Controller.java
+++ Events/src/sample/Controller.java
@@ -1,15 +1,55 @@
 package sample;
 
-import javafx.event.Event;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 
 public class Controller {
     @FXML
     private TextField nameField;
+    @FXML
+    private Button helloButton;
+    @FXML
+    private Button byeButton;
+    @FXML
+    private CheckBox clearCheckbox;
 
     @FXML
-    public void onButtonClicked(){
-        System.out.println("Hello, " + nameField.getText());
+    public void initialize(){
+        helloButton.setDisable(true);
+        byeButton.setDisable(true);
+    }
+
+    @FXML
+    public void onButtonClicked(ActionEvent e){
+        if(e.getSource().equals(helloButton)){
+            System.out.println("Hello, " + nameField.getText());
+            handleTextClearCheck();
+        } else if(e.getSource().equals(byeButton)){
+            System.out.println("Bye, " + nameField.getText());
+            handleTextClearCheck();
+        }
+//        try {
+//            Thread.sleep(10000);
+//        } catch(InterruptedException event){
+//            //dont care
+//        }
+    }
+
+    public void handleTextClearCheck(){
+        if(clearCheckbox.isSelected()){
+            nameField.clear();
+            handleKeyReleased();
+        }
+    }
+
+    @FXML
+    public void handleKeyReleased(){
+        String text = nameField.getText();
+        boolean disableButtons = text.isEmpty() || text.trim().isEmpty();
+        helloButton.setDisable(disableButtons);
+        byeButton.setDisable(disableButtons);
     }
 }
