b65cbd778d9d0cae7fd74ebc733992f64d0ade57
Griffin Poole <griffinpoole5@gmail.com>
2020-04-24 11:51:53 -0400

    Added results processing to fire when OK button Pressed on new List Item

--- TodoList/src/com/griffinpoole/todolist/Controller.java
+++ TodoList/src/com/griffinpoole/todolist/Controller.java
@@ -16,6 +16,7 @@ import java.time.Month;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class Controller {
     private List<TodoItem> todoItems;
@@ -66,14 +67,27 @@ public class Controller {
     public void showNewItemDialog() {
         Dialog<ButtonType> dialog = new Dialog<>();
         dialog.initOwner(mainBorderPane.getScene().getWindow());
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("todoItemDialog.fxml"));
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("todoItemDialog.fxml"));
-            dialog.getDialogPane().setContent(root);
+            dialog.getDialogPane().setContent(fxmlLoader.load());
         } catch(IOException e){
             System.out.println("Couldn't Load the Dialog");
             e.printStackTrace();
             return;
         }
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+
+        Optional<ButtonType> result = dialog.showAndWait();
+        if(result.isPresent() && result.get() == ButtonType.OK){
+            DialogController controller = fxmlLoader.getController();
+            controller.processResults();
+            System.out.println("OK Pressed");
+        } else {
+            System.out.println("Cancel Pressed");
+        }
+
     }
 
 
--- TodoList/src/com/griffinpoole/todolist/DialogController.java
+++ TodoList/src/com/griffinpoole/todolist/DialogController.java
@@ -1,4 +1,28 @@
 package com.griffinpoole.todolist;
 
+import com.griffinpoole.todolist.datamodel.TodoData;
+import com.griffinpoole.todolist.datamodel.TodoItem;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.time.LocalDate;
+
 public class DialogController {
+    @FXML
+    private TextField shortDescriptionField;
+    @FXML
+    private TextArea detailsArea;
+    @FXML
+    private DatePicker deadlinePicker;
+
+    public void processResults(){
+        String shortDesc = shortDescriptionField.getText().trim();
+        String details = detailsArea.getText().trim();
+        LocalDate deadline = deadlinePicker.getValue();
+
+        TodoData.getInstance().addTodoItem(new TodoItem(shortDesc, details, deadline));
+    }
+
 }
