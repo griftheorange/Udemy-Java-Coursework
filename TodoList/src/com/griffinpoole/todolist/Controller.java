eb8943406875586d0442360d877eeafe61149960
Griffin Poole <griffinpoole5@gmail.com>
2020-04-24 12:15:39 -0400

    Used Data Binding to link the TodoData singleton instance to the listview

--- TodoList/src/com/griffinpoole/todolist/Controller.java
+++ TodoList/src/com/griffinpoole/todolist/Controller.java
@@ -54,7 +54,7 @@ public class Controller {
             }
         });
 
-        todoListView.getItems().setAll(TodoData.getInstance().getTodoItems());
+        todoListView.setItems(TodoData.getInstance().getTodoItems());
         todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         todoListView.getSelectionModel().selectFirst();
     }
@@ -81,11 +81,7 @@ public class Controller {
         if(result.isPresent() && result.get() == ButtonType.OK){
             DialogController controller = fxmlLoader.getController();
             TodoItem newItem = controller.processResults();
-            todoListView.getItems().setAll(TodoData.getInstance().getTodoItems());
             todoListView.getSelectionModel().select(newItem);
-            System.out.println("OK Pressed");
-        } else {
-            System.out.println("Cancel Pressed");
         }
 
     }
