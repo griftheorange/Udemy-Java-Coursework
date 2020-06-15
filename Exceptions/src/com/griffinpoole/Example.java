372d5fd1c25f8f0e8683ea83bb4c715840799b78
Griffin Poole <griffinpoole5@gmail.com>
2020-06-15 17:37:11 -0400

    Added exception catching for user input recieving

---
+++ Exceptions/src/com/griffinpoole/Example.java
@@ -0,0 +1,28 @@
+package com.griffinpoole;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Example {
+    public static void main(String[] args) {
+        int result = divide();
+        System.out.println(result);
+    }
+
+    private static int divide(){
+        int x = getInt();
+        int y = getInt();
+        System.out.println("x is " + x + ", y is " + y);
+        return x / y;
+    }
+
+    private static int getInt(){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter an Integer");
+        try {
+            return s.nextInt();
+        } catch(InputMismatchException){
+
+        }
+    }
+}
