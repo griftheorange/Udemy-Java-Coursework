616f01a93f1e920ff21d18cefb7731657776399f
Griffin Poole <griffinpoole5@gmail.com>
2020-06-17 15:55:18 -0400

    Caught exceptions and threw new exceptions

--- Exceptions/src/com/griffinpoole/Example.java
+++ Exceptions/src/com/griffinpoole/Example.java
@@ -1,6 +1,7 @@
 package com.griffinpoole;
 
 import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Example {
@@ -10,19 +11,32 @@ public class Example {
     }
 
     private static int divide(){
-        int x = getInt();
-        int y = getInt();
+        int x, y;
+        try {
+            x = getInt();
+            y = getInt();
+        } catch(NoSuchElementException e){
+            throw new ArithmeticException("no suitable input");
+        }
         System.out.println("x is " + x + ", y is " + y);
-        return x / y;
+        try {
+            return x / y;
+        } catch (ArithmeticException e) {
+            throw new ArithmeticException("attempt to divide by zero");
+        }
     }
 
     private static int getInt(){
         Scanner s = new Scanner(System.in);
         System.out.println("Please enter an Integer");
-        try {
-            return s.nextInt();
-        } catch(InputMismatchException){
-
+        while(true){
+            try {
+                return s.nextInt();
+            } catch(InputMismatchException e){
+                // Go round again
+                s.nextLine();
+                System.out.println("Please enter a number using only the digits 0-9");
+            }
         }
     }
 }
